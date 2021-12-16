7021fd8fc7babb321aae15f2780c5c6196b12975
SebC99 <sebastien.caron@gmail.com>
2021-12-16 22:19:52 +0100

    improvement

--- index.js
+++ index.js
@@ -11,33 +11,41 @@ const citiesMap = cities.reduce((acc, city) => {
 module.exports = function (query) {
   const text = clean(query);
   const words = text.split(/\s/);
-
   const results = [];
+
   for (let city of citiesMap.keys()) {
     if (city.has(text)) {
       const result = citiesMap.get(city);
       result.exactMatch = clean(result.name) == text;
       result.otherWords = undefined;
       result.matchName = text.split(/\s/).map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
+      result.matchPercent = 100;
       results.push(result);
     }
   }
   for (let city of citiesMap.keys()) {
-    const match = words.some(w => city.has(w));
-    if (match) {
-      const result = citiesMap.get(city);
-      if (results.some(res => res.cityId == result.cityId)) {
-        continue;
+    // generate all variations of words by reducing length, keeping the order
+    inverted_loop: {
+      for (let i = words.length - 1; i > 0; i--) {
+        for (let j = 0; j <= words.length - i; j++) {
+          const word = words.slice(j, j + i).join(' ');
+          if (city.has(word)) {
+            const result = citiesMap.get(city);
+            if (results.some(res => res.cityId == result.cityId)) {
+              continue;
+            }
+            result.exactMatch = false;
+            result.otherWords = [...words.slice(0, j), ...words.slice(j+i)];
+            result.matchName = word.split(/\s/).map((w) => w.charAt(0).toUpperCase() + w.substring(1)).join(' ');
+            result.matchPercent = 100 * i / words.length;
+            results.push(result);
+            break inverted_loop;
+          }
+        }
       }
-      result.exactMatch = false; //words.includes(clean(result.name));
-      result.otherWords = words.filter(w => !city.has(w));
-      result.matchName = words.find(w => city.has(w)).split(/\s/).map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
-      results.push(result);
     }
   }
   return results
     .map(({country, ...rest}) => ({country: countryCodes.find(({ISO}) => ISO == country), ...rest}))
     .sort((a,b) => (b.exactMatch - a.exactMatch) || (b.population - a.population));
 }
-
-// module.exports("Amalfi");
