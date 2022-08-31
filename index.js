ee3dd3980d77ba6c3e06086aef0fe82be83a7135
SebC99 <sebastien.caron@gmail.com>
2022-08-31 22:04:11 +0200

    remove for loop of sets

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const countryCodes = require("./countryCodes.json");
 const clean = (text) => text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[.,\/#!$%\^&\*;:{}=\-_`~(){}\[\]]/g," ").replace(/\s\s+/g, ' ').trim().toLowerCase();
 const citiesMap = cities.reduce((acc, city) => {
   const set = new Set([clean(city.name), ...city.altName.map(alt => clean(alt).replace(/ *\[[^]*\] */g, ""))]);
-  acc.set(set, city);
+  for (const name of set) {
+    const cities = acc.get(name) || [];
+    cities.push(city);
+    acc.set(name, cities)
+  }
   return acc;
 }, new Map());
 
@@ -13,39 +17,39 @@ module.exports = function (query) {
   const words = text.split(/\s/).slice(0,10);
   const results = [];
 
-  for (let city of citiesMap.keys()) {
-    if (city.has(text)) {
-      const result = citiesMap.get(city);
+  
+  if (citiesMap.has(text)) {
+    const cities = citiesMap.get(city);
+    cities.forEach((result) => {
       result.exactMatch = clean(result.name) == text;
-      result.otherWords = undefined;
-      result.matchName = text.split(/\s/).map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
       result.matchPercent = 100;
-      results.push(result);
-    }
+    })
+    results.push(...cities);
   }
-  for (let city of citiesMap.keys()) {
-    // generate all variations of words by reducing length, keeping the order
-    inverted_loop: {
-      for (let i = words.length - 1; i > 0; i--) {
-        for (let j = 0; j <= words.length - i; j++) {
-          const word = words.slice(j, j + i).join(' ');
-          if (city.has(word)) {
-            const result = citiesMap.get(city);
+  
+  // generate all variations of words by reducing length, keeping the order
+    const length = words.length
+    for (let i = length - 1; i > 0; i--) {
+      for (let j = 0; j <= length - i; j++) {
+        var word = words[j];
+        for (var l = j + 1; l < j + i; l++){
+          word += " " + words[l];
+        }
+
+        if (citiesMap.has(word)) {
+          const cities = citiesMap.get(word);
+          cities.forEach((result) => {
             if (results.some(res => res.cityId == result.cityId)) {
-              continue;
+              return;
             }
             result.exactMatch = false;
-            result.otherWords = [...words.slice(0, j), ...words.slice(j+i)];
-            result.matchName = word.split(/\s/).map((w) => w.charAt(0).toUpperCase() + w.substring(1)).join(' ');
-            result.matchPercent = 100 * i / words.length;
+            result.matchPercent = 100 * i / length;
             results.push(result);
-            break inverted_loop;
-          }
-        }
+          });
       }
     }
   }
   return results
     .map(({country, ...rest}) => ({country: countryCodes.find(({ISO}) => ISO == country), ...rest}))
-    .sort((a,b) => (b.exactMatch - a.exactMatch) || (b.population - a.population));
+    .sort((a,b) => (b.exactMatch - a.exactMatch) || (b.matchPercent - a.matchPercent) || (b.population - a.population));
 }
